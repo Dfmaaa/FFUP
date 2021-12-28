6aaf45845c0c088291899ee801d663bf9382694c
Sameer Achhab <sameerkw23@gmail.com>
2021-12-28 21:05:21 +0530

    Delete bin directory

--- bin/bin/commands.java
+++
@@ -1,9 +0,0 @@
-package bin;
-import java.io.File;
-public class commands{
-     public static void run(String s){
-     for( File f : new File("bin").listFiles()){
-         System.out.println(f.getName().replaceFirst("[.][^.]+$", ""));
-     }
-    }
-}
