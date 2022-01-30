2dd2ae3d285f5fd9a382eae4815967fea94649cb
Sameer Achhab <sameerkw23@gmail.com>
2022-01-30 16:57:03 +0530

    fixed bug
    the bug was that when deleting anything, after deleting it, it claimed that the file didn't exist, which is technically true but can be confusing for some users, so made the if condition an else if condition

---
+++ src/bin/remove.java
@@ -0,0 +1,38 @@
+package bin;
+import java.io.File;
+public class remove{
+ private static void recursive(File a){
+    for(File f : a.listFiles()){
+     if(f.isDirectory()){
+        recursive(f);
+     }
+     f.delete();
+    }
+    a.delete();
+ }
+
+ public static void run(String s){
+  File[] arr=new File[s.split(" ").length-1];
+  for(int i=0;i<arr.length;i++){
+   arr[i]=new File(s.split(" ")[i+1]);
+  }
+  if(s.split(" ")[1].equals("*")){
+    arr=new File(".").listFiles();
+    }
+    for(File f : arr){
+     try {
+         if (f.isDirectory()) {
+             recursive(f);
+         } else if (f.isFile()) {
+             f.delete();
+         }
+         else if(f.exists()==false){
+             System.out.println(f.getAbsolutePath() + " doesn't exist.");
+         }
+     }
+     catch(Exception e){
+         System.out.println("Could not delete " + f.getAbsolutePath());
+     }
+    }
+ }
+}
