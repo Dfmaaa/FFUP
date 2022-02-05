6bbfefff0571d18e3a501d986ca8b075ae528824
Sameer Achhab <sameerkw23@gmail.com>
2022-02-05 22:28:56 +0530

    Update cdir.java

--- src/bin/cdir.java
+++ src/bin/cdir.java
@@ -3,7 +3,7 @@ import java.io.File;
 import java.io.IOException;
 public class cdir{
  public static void run(String s) throws IOException{
-  String[] files=s.substring(7,s.length()).split(" ");
+  String[] files=s.substring(5,s.length()).split(" ");
    for(String f : files){
     new File(f).mkdir();
    }
